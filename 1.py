d624b17be0161ea6b0089febd1c529cd343d6cae
Laurent Dietrich <dietrich.laurent@gmail.com>
2024-12-01 20:34:23 +0100

    cleanup

--- 1.py
+++ 1.py
@@ -1,5 +1,4 @@
 ## P1
-
 with open('1','r') as f:
     L1 = [ ]
     L2 = [ ]
@@ -10,11 +9,10 @@ with open('1','r') as f:
 
 L1.sort()
 L2.sort()
-
 print('Part 1 : ', sum( [abs(L1[i]-L2[i]) for i in range(len(L1))]   ))
 
 ## P2
 sim = 0
 for x in set(L1):
-    sim += x*sum( [y==x for y in L2]   )
+    sim += x*L2.count(x)
 print('Part 2 : ', sim)
